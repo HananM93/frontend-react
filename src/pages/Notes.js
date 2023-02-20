e79b554c431864024806cff0bf4a916b02122ebd
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2023-02-20 15:56:30 -0600

    updated notes.js

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,11 +1,18 @@
-import React from 'react'
-
-const Notes = () => {
+import React from "react";
+import { Card } from "react-bootstrap";
+const Notes = ({ notes }) => {
   return (
     <div>
-      
+      {notes.map((note) => (
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>{note.title}</Card.Title>
+            <Card.Text>{note.text}</Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Notes
+export default Notes;
