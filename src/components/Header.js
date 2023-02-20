27d8f7dfeaff7ac90824a3bf1666ee577aa5ccd3
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2023-02-20 15:16:50 -0600

    modified header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,16 +7,15 @@ const Header = () => {
     <div>
       <Navbar bg="light" expand="lg">
         <Container>
-          {/* <Navbar.Brand href="#home">MentNotes</Navbar.Brand> */}
-          <Navbar.Brand >MentNotes</Navbar.Brand>
+          
+          <Navbar.Brand href="/">MentNotes</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link>Home</Nav.Link>
-              {/* <Nav.Link href="#home">Home</Nav.Link> */}
-              <Nav.Link>Notes</Nav.Link>
-              <Nav.Link>Create</Nav.Link>
-              {/* <Nav.Link href="#link">Link</Nav.Link> */}
+              
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/notes">Notes</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
