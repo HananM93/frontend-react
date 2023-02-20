83e2d47590253b36698c3ab5bd153ca3ec378978
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2023-02-20 15:46:06 -0600

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "bootswatch/dist/solar/bootstrap.min.css";
+import { useState } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
@@ -7,13 +8,14 @@ import Home from "./pages/Home";
 import Notes from "./pages/Notes";
 
 function App() {
+  const [notes,setNotes] = useState([])
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/notes" element={<Notes />} />
+        <Route path="/" element={<Home  />} />
+        <Route path="/create" element={<Create setNotes={setNotes} />} />
+        <Route path="/notes" element={<Notes setNotes={notes} />} />
       </Routes>
     </div>
   );
