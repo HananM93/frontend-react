e9b0f3f1e527a11e1345e0ba6c7627529692a6ca
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2023-02-20 15:10:21 -0600

    imported file pages and create routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import 'bootswatch/dist/solar/bootstrap.min.css';
-import './App.css';
-import Header from './components/Header';
+import "bootswatch/dist/solar/bootstrap.min.css";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Create from "./pages/Create";
+import Home from "./pages/Home";
+import Notes from "./pages/Notes";
 
 function App() {
   return (
     <div className="App">
-     <Header />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/notes" element={<Notes />} />
+      </Routes>
     </div>
   );
 }
