dc361afe9ac19f5dbd532bc83a03234ad49b71f7
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2023-02-20 15:42:27 -0600

    modified create page

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,19 +1,39 @@
 import React from "react";
 import { useState } from "react";
 import { Container, Form } from "react-bootstrap";
-const Create = () => {
+const Create = ({setNotes}) => {
+  const [form, setForm] = useState({ title: "", text: "" });
+
+  function handleChange(event) {
+    const {value,name} = event.target
+    setForm({...form, [name]:value })
+    
+  }
   
+
   return (
     <Container>
-      <Form className="mt-5" style={{width:' 45%'}}>
+      <Form className="mt-5" style={{ width: " 45%" }}>
         <Form.Group className="mb-3">
           <Form.Label className="">Title</Form.Label>
-          <Form.Control type="email" placeholder="Enter title" />
+          <Form.Control
+            onChange={handleChange}
+            value={form.title}
+            name="title"
+            type="email"
+            placeholder="Enter title"
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" >
+        <Form.Group className="mb-3">
           <Form.Label>Text</Form.Label>
-          <Form.Control type="text" placeholder="Enter text" />
+          <Form.Control
+            onChange={handleChange}
+            value={form.text}
+            name="text"
+            type="text"
+            placeholder="Enter text"
+          />
         </Form.Group>
       </Form>
     </Container>
